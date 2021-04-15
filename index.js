092714495e565cbc905c846419753987d7cf7909
Josep <albiol957@hotmail.com>
2021-04-15 18:10:36 +0200

    sin bonus

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[]
+    this.length= this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    if(this.length === undefined){
+      this.items.push(item);
+    } else {
+        this.items.push(item);
+        this.items.sort(function(a, b) {
+        return a - b;
+      });
+    }
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefinded){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length === undefinded){
+      throw new Error('EmptySortedList');
+    } else {
+      this.items.reduce(function(a, b) {
+        return Math.max(a, b);
+    });
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length === undefinded){
+      throw new Error('EmptySortedList');
+    } else {
+        return Math.min(...this.items);
+    }
+  }
+  
 
-  sum() {}
+  sum() {
+    if(this.length === undefined){
+      return 0;
+    }
+  }
 
   avg() {}
 }
